662058819e288b9f03fb48b78f7c6425a6405b89
ByronGBP <byrongbp@gmail.com>
2017-09-04 19:45:44 +0200

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,115 @@
+
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+
+    this.direction = 0;
+    this.nextFloor = 0;
+    this.waitingList = [];
+    this.passengers = [];
+
+    this.timer = null;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    if (!this.timer){
+      this.nextFloor = this.requests.shift();
+      console.log(this.nextFloor);
+      this.timer = setInterval(() => this.update(), 1000);
+    }
+  }
+
+  stop() {
+    if (this.timer){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+  }
+
+  update() {
+    this._updateFloor();
+    this.log();
+   }
+
+   _updateFloor() {
+     if (this.timer){
+       if (this.nextFloor !== this.floor){
+         if(this.nextFloor < this.floor){
+           this.floorDown();
+         }
+         else if (this.nextFloor > this.floor) {
+           this.floorUp();
+         }
+       }
+       else {
+         this._checkPassengers();
+         if (this.requests.length > 0){
+           this.nextFloor = this.requests.shift();
+         }
+         else{
+           this.stop();
+         }
+       }
+     }
+   }
+
+   _checkPassengers() {
+     if (this.waitingList.length > 0){
+       this.waitingList.forEach((person, index) => {
+         if (person.originFloor === this.floor){
+           this._passengersEnter(person, index);
+         }
+       });
+     }
+
+     if(this.passengers.length > 0){
+       this.passengers.forEach( (person, index) => {
+         if (person.destinationFloor === this.floor) {
+           this._passengersLeave(person, index);
+         }
+       });
+     }
+   }
+
+  _passengersEnter(person, i) {
+    this.passengers.push(person);
+    this.waitingList.splice(i, 1);
+    console.log(`${person.name} just enter!`);
+    this.requests.push(person.destinationFloor);
+   }
+
+  _passengersLeave(person, i) {
+    console.log(`${person.name} just leave!`);
+    this.passengers.splice(i,1);
+  }
+
+  floorUp() {
+    if(this.floor < this.MAXFLOOR){
+      this.floor += 1;
+    }
+  }
+
+  floorDown() {
+    if (this.floor > 0){
+      this.floor -= 1;
+    }
+   }
+
+  call(person) {
+    this.waitingList.push(person);
+    this.requests.push(person.originFloor);
+    if(!this.timer){
+      this.start();
+    }
+  }
+
+  log() {
+    console.log(this.floor, this.direction);
+  }
 }
+
+var newElevator  =  new Elevator();
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,17 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+let byron = new Person("Byron", 0, 2);
+let luis = new Person("Luis", 1, 7);
+let markus = new Person("Markus", 2, 5);
+let jennie = new Person("Jennie", 4, 9);
+let tania = new Person("Tania", 7, 1);
+
+let elevator = new Elevator();
+
+elevator.call(byron);
+elevator.call(luis);
+elevator.call(markus);
+elevator.call(jennie);
+elevator.call(tania);
